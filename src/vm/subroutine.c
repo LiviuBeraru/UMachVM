5e0a381527c2190b00d6db39a0d8ca17c0376a3b
Liviu Beraru <beraru.liviu@googlemail.com>
2012-09-05 23:11:11 +0200

    Call und Return
    Die Befehle 'call' und 'ret' funktionieren.
    (so scheint es zumindest... :-)

--- src/vm/subroutine.c
+++ src/vm/subroutine.c
@@ -1,19 +1,34 @@
+#include "memory.h"
+#include "registers.h" // register names
+#include "branch.h"    // core_jmp
+
+
 #include <stdio.h>
 
-int core_go   (void)
+
+int core_go(void)
 {
     printf("%s\n", __func__);
     return 0;
 }
 
-int core_call (void)
+// call is like branching but does more
+int core_call(void)
 {
-    printf("%s\n", __func__);
+    /* save the current PC */
+    if (mem_push(registers[PC].value) == -1) { return -1; }
+    if (core_jmp() == -1) { return -1; }
+
     return 0;
 }
 
-int core_ret  (void)
+int core_ret(void)
 {
-    printf("%s\n", __func__);
+    int32_t pc = 0;
+    if (mem_pop(&pc) == -1) { return -1; }
+    
+    /* restore the last saved PC */
+    registers[PC].value = pc;
+    
     return 0;
 }
