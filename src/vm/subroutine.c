73d6b2e49e41eeb5ad66e3ba06f7f3cf722e6438
Liviu Beraru <beraru.liviu@googlemail.com>
2012-10-20 02:33:30 +0200

    Befehl GO und mehr infos in interrupt()

--- src/vm/subroutine.c
+++ src/vm/subroutine.c
@@ -1,18 +1,49 @@
 #include "memory.h"
-#include "registers.h" // register names
-#include "branch.h"    // core_jmp
+#include "registers.h"  // register names
+#include "branch.h"     // core_jmp
+#include "core.h"       // instruction[4]
+#include "system.h"     // interrupt()
+#include "interrupts.h" // interrupt numbers
 
-
-#include <stdio.h>
-
-
+/**
+ * Implements the GO command.
+ */
 int core_go(void)
 {
-    printf("%s\n", __func__);
+    /* register number is the second byte of the instruction, 
+     * after the opcode */
+    int32_t regno = instruction[1];
+    int32_t regval = 0;
+    /* try to read the register with number regno */
+    if (read_register(regno, &regval) == -1) { return -1; }
+    
+    /* check if the register value contains an address to executable code.
+     * executable code (the code segment) is the memory chunk between 
+     * address 0 and the begin of the data segment, which is stored in the 
+     * register DS. If the register value contains an address outside of the
+     * code segment, we interrupt with segmentation fault.
+     */
+    
+    if (regval < 0 || regval >= registers[DS].value) {
+        interrupt(INT_SEGFAULT);
+        return -1;
+    }
+    
+    /* set the PC register to the register value */
+    registers[PC].value = regval;
+    
+    /* because the machine adds 4 to the PC after executing this
+     * instruction, we must adjust the value by subtracting 4
+     */
+    registers[PC].value -= 4;
+    
     return 0;
 }
 
-// call is like branching but does more
+/** 
+ * Implemets the CALL command.
+ * Pushes the current PC value and the calls JMP.
+ */
 int core_call(void)
 {
     /* save the current PC */
@@ -22,6 +53,12 @@ int core_call(void)
     return 0;
 }
 
+/** 
+ * Implements the RET command. 
+ * What this function do is to just pop from stack into 
+ * the register PC. This assumes that the return address from 
+ * a subroutine was previously pushed on the stack.
+ */
 int core_ret(void)
 {
     int32_t pc = 0;
--- src/vm/system.c
+++ src/vm/system.c
@@ -1,16 +1,25 @@
-#include "core.h"
+#include "core.h"      // instruction[]
 #include "logmsg.h"
+#include "registers.h" // for value of PC
+#include "disassemble.h" // disassemble current instruction 
 
 int interrupt(int number)
 {
-    logmsg(LOG_WARN, "Interrupt %d", number);
+    char dis_instr[128] = { '\0' };
+    disassemble(instruction, dis_instr, 0); // 0 for don't print hex
+    
+    logmsg(LOG_INFO, "Instruction <%s> at PC = %d caused Interrupt %d", 
+           dis_instr, registers[PC].value, number);
     running = 0;
     return 0;
 }
 
 int core_int(void)
 {
-    int8_t b = instruction[1];
+    int8_t b = instruction[1]; // last byte of the instruction
+    /* reconstruct the 3 bytes interrupt number, 
+     * which is stored in big endian order inside the instruction,
+     * on this little endian intel machine */
     int ino = (b << 16) | (instruction[2] << 8) | (instruction[3]);
     return (interrupt(ino));
 }
