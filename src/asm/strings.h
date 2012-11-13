efa4748cea5803afa7ba4dccaf3a8aff8dc72aa4
initzero <initzero@gmail.com>
2012-11-13 19:28:03 +0100

    Removed the legacy assembler

--- src/asm/strings.h
+++
@@ -1,8 +0,0 @@
-#ifndef STRINGS_H
-#define STRINGS_H
-
-int split(char *string, const char *pattern, char *items[], size_t max);
-int str_to_int(const char *number, long *n);
-char *str_trim(char *string);
-
-#endif 
